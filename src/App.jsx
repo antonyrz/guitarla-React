fef6761d733961cb7ec6044feb77f6e3a19cd898
Antonyhz <hernandezantony333@gmail.com>
2025-07-03 16:06:14 -0400

    primer state

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Header from './components/Header'
 import Guitar from './components/Guitar'
 
 function App() {
+
+  // State
+  const [auth, setAuth] = useState(false);
+
+  console.log(auth);
+
   return (
     <>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 export default Header
+import { useState } from "react"
+
+
 
 function Header(){
 
