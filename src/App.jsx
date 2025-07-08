0b10e1d0895378ed6be19c2a79b71847f16ef602
Antonyhz <hernandezantony333@gmail.com>
2025-07-08 16:35:13 -0400

    Juntando Prop con state

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,24 @@ function App() {
 
   // State
   const [data, setData] = useState(db);
- console.log(db);
 
   return (
     <>
 
     <Header />
 
-
-
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-           <Guitar />
+
+            {data.map((guitar) => (
+              <Guitar 
+                key={guitar.id}
+                guitar={guitar}
+              />
+            ))}
+
         </div>
     </main>
 
